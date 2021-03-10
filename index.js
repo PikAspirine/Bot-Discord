c46175ab4a13d946e175b86228d32cfadceab470
PikAspirine <80416292+PikAspirine@users.noreply.github.com>
2021-03-10 18:26:56 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = Discord.Client();
+
+const fs = require('fs');
+const config = require('./config.json')
+
+
+
+
+
+
+
+
+client.login(confiprocess.env.token);
